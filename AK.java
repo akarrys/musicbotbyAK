ad8a8f088f05e4d7ee8586c339fb938c1c1d4d06
AK <artamiskarrys47@gmail.com>
2022-03-11 15:41:26 -0700

    Add files via upload

---
+++ AK.java
@@ -0,0 +1,138 @@
+package com.github.akarrys.musicbotbyAK;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.VoiceChannel;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.exceptions.RateLimitedException;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.managers.AudioManager;
+
+import javax.security.auth.login.LoginException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static net.dv8tion.jda.api.requests.GatewayIntent.GUILD_MESSAGES;
+import static net.dv8tion.jda.api.requests.GatewayIntent.GUILD_VOICE_STATES;
+
+
+public class AK extends ListenerAdapter {
+
+    public static void main(String[] args) throws IllegalArgumentException, LoginException, RateLimitedException {
+        JDABuilder.createDefault(args[0]) // Use token provided as JVM argument
+                .addEventListeners(new AK()) // Register new MusicBot instance as EventListener
+                .build(); // Build JDA - connect to discord
+    }
+       private final AudioPlayerManager playerManager;
+       private final Map<Long, GuildMusicManager> musicManagers;
+
+
+       private AK() {
+
+           this.musicManagers = new HashMap<>();
+
+           this.playerManager = new DefaultAudioPlayerManager();
+           AudioSourceManagers.registerRemoteSources(playerManager);
+           AudioSourceManagers.registerLocalSource(playerManager);
+
+       }
+
+    private synchronized GuildMusicManager getGuildAudioPlayer(Guild guild) {
+        long guildId = Long.parseLong(guild.getId());
+        GuildMusicManager musicManager = musicManagers.get(guildId);
+
+        if (musicManager == null) {
+            musicManager = new GuildMusicManager(playerManager);
+            musicManagers.put(guildId, musicManager);
+        }
+
+        guild.getAudioManager().setSendingHandler(musicManager.getSendHandler());
+
+        return musicManager;
+    }
+
+
+    @Override
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        String[] command = event.getMessage().getContentRaw().split(" ", 2);
+
+        if ("!".equals(command[0]) && command.length == 2) {
+            loadAndPlay(event.getChannel(), command[1]);
+        } else if ("!skip".equals(command[0])) {
+            skipTrack(event.getChannel());
+        }
+
+        super.onGuildMessageReceived(event);
+    }
+
+    private void loadAndPlay(final TextChannel channel, final String trackUrl) {
+        GuildMusicManager musicManager = getGuildAudioPlayer(channel.getGuild());
+
+        playerManager.loadItemOrdered(musicManager, trackUrl, new AudioLoadResultHandler() {
+            @Override
+            public void trackLoaded(AudioTrack track) {
+                channel.sendMessage("Adding to Queue" + track.getInfo().title).queue();
+                play(channel.getGuild(), musicManager, track);
+
+            }
+
+            @Override
+            public void playlistLoaded(AudioPlaylist playlist) {
+                AudioTrack firstTrack = playlist.getSelectedTrack();
+
+                if (firstTrack == null) {
+                    firstTrack = playlist.getTracks().get(0);
+                }
+                channel.sendMessage("Adding to queue " + firstTrack.getInfo().title + " (first track of playlist " + playlist.getName() + ")").queue();
+
+                play(channel.getGuild(), musicManager, firstTrack);
+
+
+            }
+
+            @Override
+            public void noMatches() {
+                channel.sendMessage("Nothing found by " + trackUrl).queue();
+            }
+
+            @Override
+            public void loadFailed(FriendlyException exception) {
+                channel.sendMessage("Could not play: " + exception.getMessage()).queue();
+            }
+
+        });
+    }
+    private void play(Guild guild, GuildMusicManager musicManager, AudioTrack track) {
+        connectToFirstVoiceChannel(guild.getAudioManager());
+
+        musicManager.scheduler.queue(track);
+    }
+
+
+    private void skipTrack(TextChannel channel) {
+        GuildMusicManager musicManager = getGuildAudioPlayer(channel.getGuild());
+        musicManager.scheduler.nextTrack();
+
+        channel.sendMessage("Skipped to next track.").queue();
+    }
+
+    private static void connectToFirstVoiceChannel(AudioManager audioManager) {
+        if (!audioManager.isConnected() && !audioManager.isAttemptingToConnect()) {
+            for (VoiceChannel voiceChannel : audioManager.getGuild().getVoiceChannels()) {
+                audioManager.openAudioConnection(voiceChannel);
+                break;
+            }
+        }
+    }
+}
+
+
+
